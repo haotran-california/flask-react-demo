5a9f4f616c237c81e139bc070c9f3223a2cc90e8
Ogriceboy <haoanhtran7@gmail.com>
2022-04-13 11:53:32 -0700

    update link

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('https://survery-demo-app.herokuapp.com/flask/hello').then(response => {
+    axios.get('https://git.heroku.com/survery-demo-app.git/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
